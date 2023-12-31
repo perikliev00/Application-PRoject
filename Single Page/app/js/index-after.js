f9e0b0a76af8862bf2a18008cb4ffed51b179bc6
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2023-12-31 15:09:37 +0200

    Delete Single Page/app directory

--- Single Page/app/js/index-after.js
+++
@@ -1,8 +0,0 @@
-if(sessionStorage.length==0) {
-    window.location.href="index.html";
-}
-let logOut=document.getElementById('log-out');
-logOut.addEventListener('click',()=> {
-    sessionStorage.clear();
-    window.location.href="index.html"
-})
